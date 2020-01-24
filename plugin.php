174522f04b3f9dd85a6a645b9dffc97ff7c53b8d
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-01-24 15:31:39 +0200

    Init

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:        FRC - WP View
+Plugin URI:         https://www.frantic.fi/
+Description:        View engine for WordPress
+Version:            0.0.1
+Author:             Frantic
+Author URI:         https://www.frantic.fi/
+*/
+
+namespace Frc\WP\Components;
+
+require_once __DIR__ . '/vendor/autoload.php';
