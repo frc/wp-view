a1465940b93514234ce7e1e5bc807502d4d8e983
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-04-27 11:13:01 +0300

    Add View

---
+++ src/View.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Frc\WP\View;
+
+use Throwable;
+
+class View
+{
+    protected $view;
+
+    protected $path;
+
+    protected $data = [];
+
+    public function __construct($view, $path, $data = [])
+    {
+        $this->view = $view;
+
+        $this->path = $path;
+
+        $this->with($data);
+    }
+
+    public function render()
+    {
+        $obLevel = ob_get_level();
+
+        ob_start();
+
+        $__data = $this->data;
+        extract($this->data, EXTR_SKIP);
+
+        try {
+            include $this->path;
+        } catch (Throwable $e) {
+            while (ob_get_level() > $obLevel) {
+                ob_end_clean();
+            }
+
+            throw $e;
+        }
+
+        return ltrim(ob_get_clean());
+    }
+
+    public function with($key, $value = null)
+    {
+        if (! is_array($key) && $value === null) {
+            $this->data = [
+                'children' => $key,
+            ];
+        } elseif (is_array($key) && ! isAssoc($key)) {
+            $this->data = [
+                'children' => "\n" . join("\n", $key) . "\n",
+            ];
+        } elseif (is_array($key)) {
+            $this->data = array_merge($this->data, $key);
+        } else {
+            $this->data[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->render();
+    }
+}
---
+++ tests/Unit/ViewTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Frc\WP\View\View;
+
+class ViewTest extends TestCase
+{
+    protected function view($view, $data = [])
+    {
+        return new View(
+            $view, // Name
+            __DIR__ . '/../stubs/views/' . $view, // Path
+            $data // Data
+        );
+    }
+
+    /** @test */
+    public function it_should_render_given_path()
+    {
+        $this->assertStringContainsString(
+            '<div>Hello World!</div>',
+            $this->view('index.php')->render()
+        );
+    }
+
+    /** @test */
+    public function it_should_render_given_path_with_data()
+    {
+        $this->assertStringContainsString(
+            '<div>Hello View</div>',
+            $this->view('data.template.php', ['title' => 'Hello View'])->render()
+        );
+    }
+
+    /** @test */
+    public function it_should_render_default_parameter_as_children()
+    {
+        $this->assertStringContainsString(
+            '<div>Hello Default Parameter</div>',
+            $this->view('children.php', 'Hello Default Parameter')->render()
+        );
+    }
+
+    /** @test */
+    public function it_should_render_array_default_parameter_as_children()
+    {
+        $this->assertStringContainsString(
+            '<p>' . PHP_EOL .
+            'This is in p-tag' . PHP_EOL .
+            '</p>' .PHP_EOL,
+            $this->view('children.php', [
+                '<p>',
+                'This is in p-tag',
+                '</p>'
+            ])->render()
+        );
+    }
+}
