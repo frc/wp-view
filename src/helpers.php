db2b62fa2ed3e13219549140d6e954cd8b578587
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-04-27 11:21:33 +0300

    Add helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,28 @@
 <?php
+
+use Frc\WP\View\Factory;
+use Frc\WP\View\FileFinder;
+
+function frc_view($view = null, $data = [])
+{
+    static $factory;
+
+    if (! $factory) {
+        $factory = new Factory(
+            new FileFinder([
+                get_theme_file_path('/views'),
+                get_parent_theme_file_path('/views'),
+            ])
+        );
+    }
+
+    if (! $view) {
+        return $factory;
+    }
+
+    return $factory->make($view, $data);
+}
+
+function isAssoc($array) {
+	return array_keys($array) !== range(0, count($array) - 1);
+}
